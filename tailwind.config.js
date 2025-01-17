b36f059ab6ad2bb460684368fc84b871ed986056
Shalom Steinbach <shalomst@monday.com>
2025-01-17 13:21:27 -0500

    update build process

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,11 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 const plugin = require("tailwindcss/plugin");
 
 module.exports = {
+  content: ["./docs/**/*.html"],
   theme: {
     /* For Letter size */
     screens: {
-      print: {"raw": "print"},
+      print: { raw: "print" },
       xsm: "500px",
       sm: "640px",
       md: "833px",
@@ -24,7 +25,7 @@ module.exports = {
     // },
     extend: {
       fontFamily: {
-        "firago": ["FiraGO", ...defaultTheme.fontFamily.sans],
+        firago: ["FiraGO", ...defaultTheme.fontFamily.sans],
       },
       fontSize: {
         sm2: "0.9375rem", // 15px label
@@ -37,43 +38,43 @@ module.exports = {
         normal: "1.34",
       },
       maxWidth: {
-        "letter": "66.40625rem",
-        "a4": "64.609375rem"
+        letter: "66.40625rem",
+        a4: "64.609375rem",
       },
       height: {
-        "letter": "85.9375rem",
+        letter: "85.9375rem",
         "letter-col": "71.625rem",
         "letter-col-full": "77.9375rem",
-        "a4": "91.350883rem",
+        a4: "91.350883rem",
         "a4-col": "77.038383rem",
         "a4-col-full": "83.350883rem",
       },
       spacing: {
-        "0.5": "2px", // 2px
-        "1.5": "0.375rem", // 6px
-        "1.6": "0.4375rem", // 7px
-        "2.1": "0.5625rem", // 9px
-        "2.5": "10px", // 10px
-        "3.2": "0.8125rem", // 16px
-        "4.5": "1.125rem", // 8px
-        "11": "2.75rem", // 44px (once)
+        0.5: "2px", // 2px
+        1.5: "0.375rem", // 6px
+        1.6: "0.4375rem", // 7px
+        2.1: "0.5625rem", // 9px
+        2.5: "10px", // 10px
+        3.2: "0.8125rem", // 16px
+        4.5: "1.125rem", // 8px
+        11: "2.75rem", // 44px (once)
       },
       colors: {
         gray: {
-          "150": "hsl(210, 44%, 97%)",
-          "250": "hsl(210, 40%, 93.5%)",
-          "550": "hsl(218, 20%, 63%)",
-          "650": "hsl(216, 15%, 48%)",
-          "750": "hsl(214, 17%, 32%)",
-          ...defaultTheme.colors.gray
+          150: "hsl(210, 44%, 97%)",
+          250: "hsl(210, 40%, 93.5%)",
+          550: "hsl(218, 20%, 63%)",
+          650: "hsl(216, 15%, 48%)",
+          750: "hsl(214, 17%, 32%)",
+          ...defaultTheme.colors.gray,
         },
-        link: "#fbf3f3"
+        link: "#fbf3f3",
       },
     },
   },
 
   variants: {
-    textColor: ["responsive", 'hover', 'focus', 'group-hover'],
+    textColor: ["responsive", "hover", "focus", "group-hover"],
     margin: ["responsive", "last", "first"],
     padding: ["responsive", "last"],
   },
@@ -81,7 +82,7 @@ module.exports = {
   plugins: [
     plugin(function ({ addBase, addUtilities, theme }) {
       addBase({
-        "body": {
+        body: {
           "-webkit-font-smoothing": "subpixel-antialiased",
         },
       });
@@ -150,13 +151,13 @@ module.exports = {
           "font-feature-settings": "'case' on",
         },
         ".hyphens-manual": {
-          "hyphens": "manual",
+          hyphens: "manual",
         },
-      }
+      };
 
       addUtilities(typographyUtils, {
         variants: ["responsive"],
-      })
+      });
 
       /**
        * Project Spicific Utilities
@@ -164,12 +165,12 @@ module.exports = {
       const projectSpecificUtils = {
         ".border-inset": {
           "box-shadow": `inset 0 0 0 1px ${theme("colors.gray.400")}`,
-        }
-      }
+        },
+      };
 
       addUtilities(projectSpecificUtils, {
         variants: ["responsive"],
-      })
+      });
 
       /**
        * CSS Multi-Column Layout Utilities
@@ -205,12 +206,11 @@ module.exports = {
         ".col-fill-balance": {
           "column-fill": "balance",
         },
-      }
+      };
 
       addUtilities(columnUtils, {
         variants: ["responsive"],
-      })
+      });
     }),
-
-  ]
+  ],
 };
